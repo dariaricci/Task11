b7783c968a73a86028dfdc12479092293518d2c4
Даша <@DESKTOP-K4NVIE4>
2022-02-17 00:14:30 +0300

    Добавьте файлы проекта.

---
+++ Task 11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solve solve = new Solve { k = Convert.ToDouble(Console.ReadLine()), b = Convert.ToDouble(Console.ReadLine())};
+            solve.Root();
+            Console.ReadKey();
+
+        }
+
+        struct Solve
+        {
+            public double k;
+            public double b;
+            public double x;
+            
+            public void Root()
+            {
+                x = -b / k;
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
